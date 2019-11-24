0de9591fe662fdca0633d26cccd168f7f08d8e41
viswanathan.kp <viswanathan.kp@swiggy.in>
2019-11-24 20:52:37 +0530

    testing travis ci

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Text('Hello Flutter'),
+        child: Text('Hello Flutter Messenger'),
       ),
     );
   }
